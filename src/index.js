14c87cb55d61e65d5706534fd904984164bbf7b5
Omer Levi <omerlevi223@gmail.com>
2022-05-25 11:40:56 +0300

    11:40

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux'
+
 import {RootCmp} from './root-cmp'
-import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
+// import {store} from './store/store'
+import { BrowserRouter as Router } from 'react-router-dom'
+// import './assets/styles/main.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
