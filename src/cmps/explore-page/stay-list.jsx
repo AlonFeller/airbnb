ba00cf014e8da28e5e217626575e4be3e9077cc6
Eldar Nir <loshinas@gmail.com>
2022-05-25 20:07:48 +0300

    fixed list promise

--- src/cmps/explore-page/stay-list.jsx
+++ src/cmps/explore-page/stay-list.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react'
 import { stayService } from '../../services/stay.service'
 
 
 
-export const  StayList = () => {
-    
-    const stays = stayService.query('stayDB')
-    console.log(stays);
-    const  leng = stays.length
+export const StayList =  () => {
+    const [ stays, setStays] = useState([])
+
+    useEffect(() => {
+        getStays()
+    }, [])
+
+    const getStays = async () => {
+         const stays = await stayService.query('stay')
+         setStays(stays)
+    }
+
+
+    console.log('stays from list', stays);
+    const leng = stays.length
 
     return (
         <section>
