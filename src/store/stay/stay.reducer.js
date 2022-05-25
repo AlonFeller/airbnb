4cb536b35e9d876fed8d15fe90c77c24bff4a6f7
Eldar Nir <loshinas@gmail.com>
2022-05-25 16:35:28 +0300

    stay reducer

---
+++ src/store/stay/stay.reducer.js
@@ -0,0 +1,34 @@
+import { stayService } from "../../services/stay.service";
+
+const initialState = { 
+    stays: [],
+    selectedStay: null,
+
+}
+
+export function stayReducer( state = initialState, action) {
+    let newState = state
+    let stays
+
+    switch (action.type) {
+        case 'SET_STAYS':
+            newState = { ...state, stays: action.stays }
+            break
+        case 'ADD_STAY':
+            newState = { ...state, stays: [ ...state.stays, action.stay] }
+            break
+        case 'UPDATE_STAY':
+            stays = state.stays.map(stay => (stay._id === action.stay._id)? action.stay : stay)
+            newState = { ...state, stays: action.stays }
+            break
+        case 'REMOVE_STAY':
+            return { ...state, stays: state.stays.filter(stay => stay._id !== action.stayId) }
+            break
+        default:
+    }
+    return newState
+
+
+
+}
+
