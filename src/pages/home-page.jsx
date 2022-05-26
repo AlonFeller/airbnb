27a814dfa987123e7274634218df9b4345f1259f
Alon Feller <alon.feller16@gmail.com>
2022-05-26 15:17:55 +0300

    homepage

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,40 +1,44 @@
 import React, { useEffect } from 'react'
 import { HashRouter as Router, Route, Link, Switch, useNavigate } from 'react-router-dom'
-
+import hero from '../assets/Images/hero.png'
 
 export function HomePage() {
 
     const navigate = useNavigate()
-
     const goTo = (path) => {
         navigate('/')
         navigate(path)
     }
 
+
+
     return (
         <>
-        <h1>
+            <section className="home-page">
+                <header> logo place holder
+                    <button onClick={() => goTo('/explore')}>explore</button>
+                    <button onClick={() => goTo('/stay')}>stay</button>
+                </header>
+                <img className="hero img" src={hero} alt="logo"></img>
+                <span className="title">Not sure where to go ? Perfect.</span>
+                <h1>
 
-            hi from homepage
-        </h1>
+                    hi from homepage
+                </h1>
 
-            <h3>hero?</h3>
+                <h3>popular destinations list?</h3>
+                <h3>top rated list?</h3>
+                <h3>property type list?</h3>
 
-            <h3>popular destinations list?</h3>
-            <h3>top rated list?</h3>
-            <h3>property type list?</h3>
-
-            <h3>picture?</h3>
+                <h3>picture?</h3>
 
 
-            <h1></h1>
+                <h1></h1>
 
-
+            </section>
 
 
 
-            <button onClick={() => goTo('/explore')}>explore</button>
-            <button onClick={() => goTo('/stay')}>stay</button>
         </>
     )
 }
