93c43fe91fa912364cf225bc39b8d92854926fe4
Omer Levi <omerlevi223@gmail.com>
2022-06-04 17:39:32 +0300

    17:39

--- src/cmps/stay-page/stay-order.jsx
+++ src/cmps/stay-page/stay-order.jsx
@@ -63,8 +63,8 @@ export const OrderNow = ({ setIsOpenModal, isOpenModal, setIsOrderModalOpen, set
     const openModal = () => {
         setIsOrderModalOpen(true)
         setTimeout(() => {
-            // closeModal()
-            // goTo()
+            closeModal()
+            goTo()
         }, 3000)
     }
 
