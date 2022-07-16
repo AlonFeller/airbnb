84e85733c4f0b37391d8f9272b3617f5a31f5418
Omer Levi <omerlevi223@gmail.com>
2022-07-16 16:44:22 +0300

    16:44

--- src/cmps/stay-page/stay-info-line.jsx
+++ src/cmps/stay-page/stay-info-line.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { updateUser } from '../../store/user/user.actions'
 import { Star, IosShare, FavoriteBorder, Favorite } from '@mui/icons-material'
+import { UserMsg } from '../general/user-msg'
 
-export const StayInfoLine = ({ stay, setIsOpenModal, isOpenModal,openMsg }) => {
+
+export const StayInfoLine = ({ stay, setIsOpenModal, isOpenModal }) => {
     const dispatch = useDispatch()
     const { user } = useSelector(storeState => storeState.userModule)
     const [likeHeart, setLikeHeart] = useState(null)
     const [savedLink, setSavedLink] = useState(null)
+    const [textMsg, setTextMsg] = useState('')
 
     useEffect(() => {
         if (savedLink && savedLink != null) {
@@ -49,31 +52,57 @@ export const StayInfoLine = ({ stay, setIsOpenModal, isOpenModal,openMsg }) => {
         setSavedLink(true)
     }
 
+    const chooseTxtMsg = () => {
+        let txtMsg = ''
+        console.log(savedLink)
+        if (savedLink) txtMsg = 'Link Copied to clipboard'
+        else if (likeHeart) txtMsg = 'Stay liked'
+        else txtMsg = 'Stay unliked'
+        setTextMsg(txtMsg)
+    }
+
+    const openMsg = () => {
+        chooseTxtMsg()
+        setTimeout(() => {
+            // closeMsg()
+        }, 3000);
+    }
+
+    const closeMsg = () => {
+        setSavedLink(false)
+        setTextMsg('')
+    }
+
     return (
-        <section className="stay-info-line flex space-between">
-            <div className="stay-info-container flex">
-                <span className="icon-star">< Star /></span>
-                <h4 className="info-rating"> {(stay.reviewScores.rating / 20).toFixed(1)} </h4>
-                <span className="info-point" > · </span>
-                <h4 className="info-reviews pointer" onClick={() => { setIsOpenModal(!isOpenModal) }}>
-                    <u>{stay.numOfReviews} reviews</u>
-                </h4>
-                <span className="info-point" > . </span>
-                {stay.host.isSuperhost && <h4 className="info-super-host"> Superhost </h4>}
-                {stay.host.isSuperhost && <span className="info-point" > . </span>}
-                <h4 className="info-host-address"><u> {stay.address.street}</u> </h4>
-            </div>
-            <div className="info-user-btns flex">
-                <div className="share-btn-container flex align-center" onClick={(event) => onCopyUrlToClipboard((event))}>
-                    <p className="details-share"  ><IosShare /></p>
-                    <p>Share</p>
+        <>
+            <section className="stay-modal">
+                {textMsg && <UserMsg textMsg={textMsg} closeMsg={closeMsg} />}
+            </section>
+            <section className="stay-info-line flex space-between">
+                <div className="stay-info-container flex">
+                    <span className="icon-star">< Star /></span>
+                    <h4 className="info-rating"> {(stay.reviewScores.rating / 20).toFixed(1)} </h4>
+                    <span className="info-point" > · </span>
+                    <h4 className="info-reviews pointer" onClick={() => { setIsOpenModal(!isOpenModal) }}>
+                        <u>{stay.numOfReviews} reviews</u>
+                    </h4>
+                    <span className="info-point" > . </span>
+                    {stay.host.isSuperhost && <h4 className="info-super-host"> Superhost </h4>}
+                    {stay.host.isSuperhost && <span className="info-point" > . </span>}
+                    <h4 className="info-host-address"><u> {stay.address.street}</u> </h4>
                 </div>
-                <div className="save-btn-container flex align-center" onClick={(event) => ToggleHeart(event, likeHeart)}>
-                    <p className="details-save" >{!likeHeart ? <FavoriteBorder /> : <Favorite />}</p>
-                    <p>Save</p>
+                <div className="info-user-btns flex">
+                    <div className="share-btn-container flex align-center" onClick={(event) => onCopyUrlToClipboard((event))}>
+                        <p className="details-share"  ><IosShare /></p>
+                        <p>Share</p>
+                    </div>
+                    <div className="save-btn-container flex align-center" onClick={(event) => ToggleHeart(event, likeHeart)}>
+                        <p className="details-save" >{!likeHeart ? <FavoriteBorder /> : <Favorite />}</p>
+                        <p>Save</p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 }
 
--- src/pages/stay-page.jsx
+++ src/pages/stay-page.jsx
@@ -10,7 +10,6 @@ import { StayInfoLine } from '../cmps/stay-page/stay-info-line'
 import { StayMap } from '../cmps/stay-page/stay-map'
 import { ReviewsModal } from '../cmps/stay-page/reviews-modal'
 import { AddReview } from '../cmps/stay-page/add-review'
-import { UserMsg } from '../cmps/general/user-msg'
 
 
 export function StayPage() {
@@ -19,7 +18,6 @@ export function StayPage() {
     const { selectedStay } = useSelector(storeState => storeState.stayModule)
     const { openOrderModal } = useSelector(storeState => storeState.orderModule)
     const [isOpenModal, setIsOpenModal] = useState(false)
-    const [textMsg, setTextMsg] = useState('')
 
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -32,42 +30,22 @@ export function StayPage() {
         }
     }, [params.id])
 
-    // const chooseTxtMsg = () => {
-    //     let txtMsg = ''
-    //     console.log(savedLink)
-    //     if (savedLink) txtMsg = 'Link Copied to clipboard'
-    //     else if (likeHeart) txtMsg = 'Stay liked'
-    //     else txtMsg = 'Stay unliked'
-    //     setTextMsg(txtMsg)
-    // }
+
 
     const onOpenReviewsModal = () => {
         if (openOrderModal) return
         setIsOpenModal(!isOpenModal)
     }
 
-    const openMsg = () => {
-        // chooseTxtMsg()
-        setTimeout(() => {
-            closeMsg()
-        }, 3000);
-    }
-
-    const closeMsg = () => {
-        // setSavedLink(false)
-        setTextMsg('')
-    }
 
     return (
         <>
             {selectedStay && <section className="stay-page">
-                {textMsg && <UserMsg textMsg={textMsg} closeMsg={closeMsg} />}
                 <h2 className="stay-name">{selectedStay.name}</h2>
                 <StayInfoLine key="stay-info-line"
                     stay={selectedStay}
                     setIsOpenModal={setIsOpenModal}
                     isOpenModal={isOpenModal}
-                    openMsg={openMsg}
                 />
                 <StayGallery key="stay-gallery" stay={selectedStay} />
                 <StayDetails key="stay-details"
@@ -95,8 +73,6 @@ export function StayPage() {
                 <section className="stay-map">
                     <h2>Where you'll be</h2>
                     <StayMap key="stay-map" stay={selectedStay} />
-                </section>
-                <section className="stay-modal">
                 </section>
             </section>}
         </>
